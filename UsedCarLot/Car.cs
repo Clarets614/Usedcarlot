301bf2cfd54892a0060741866d2c88df49ec63e9
Clare S <Clare.Stock@gmail.com>
2024-05-14 15:29:19 -0400

    first commit- Clare Stock

---
+++ UsedCarLot/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsedCarLot
+{
+    public class Car
+    {
+        //properties
+        public string make {  get; set; }
+        public string model { get; set; }
+        public int year { get; set; }
+        public decimal price { get; set; }
+
+        //constructor 
+        public Car (string _make, string _model, int _year, decimal _price)
+        {
+            make = _make;
+            model = _model;
+            year = _year;
+            price = _price;
+
+        }
+
+        public Car()
+        {
+            make = "";
+            model = "";
+            year = -1;
+            price = -1;
+        }
+
+        public override string ToString()
+        {
+            return $"{make} {model} {year} ${price}";
+        }
+
+    }
+}
---
+++ UsedCarLot/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using UsedCarLot;
+
+Car car = new Car("Buick", "LeSabre", 1989, 2000);
+Console.WriteLine(car.ToString());
