4380924b960ea5a50cab1315a9c0cc530ec93a8e
Ben Jones <benjones1864@gmail.com>
2024-05-14 15:58:08 -0400

    added UsedCar class

--- UsedCarLot/Program.cs
+++ UsedCarLot/Program.cs
@@ -1,5 +1,16 @@
 ﻿
 using UsedCarLot;
 
-Car car = new Car("Buick", "LeSabre", 1989, 2000);
-Console.WriteLine(car.ToString());
+List<Car> carList = new List<Car>() {
+    new UsedCar("Batmobile", "Wayne Enterprises", 2024, 10000000, 200000),
+    new Car("Buick", "LeSabre", 1989, 2000),
+    new Car("Ferrari", "Enzo", 2023, 150000),
+    new UsedCar("Ford", "Focus", 2007, 1500, 50000),
+    new Car("Jeep", "Cherokee", 2024, 38000),
+    new UsedCar("Honda", "Civic", 2011, 10000, 115000)
+};
+
+foreach(var car in carList)
+{
+    Console.WriteLine(car.ToString());
+}
---
+++ UsedCarLot/UsedCar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsedCarLot
+{
+    public class UsedCar : Car
+    {
+        public double Mileage { get; set; }
+
+        public UsedCar(string _make, string _model, int _year, decimal _price, double _mileage)
+            : base(_make, _model, _year, _price)
+        { 
+            Mileage = _mileage;
+        }
+
+        public override string ToString()
+        {
+            return $"{make} {model} {year} {Mileage} miles ${price}";
+        }
+    }
+
+
+}
